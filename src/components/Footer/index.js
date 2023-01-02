3d2364e6bd116b4e7d8c77c65fb2a5be45c643a6
awgore99 <awgore99@gmail.com>
2023-01-02 16:58:51 -0600

    Added foot component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+export default class Footer extends Component {
+  render() {
+    return (
+      <footer>
+      <div>
+        <h3>Austin Gore 2023</h3>
+      </div>
+    </footer>
+    );
+  }
+}
